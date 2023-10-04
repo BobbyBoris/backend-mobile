61edf35778a76f33eed87ecceaf2ee47da4b6c08
Bobby Boris <bobbyboris26@gmail.com>
2023-10-04 11:34:01 +0700

    First Commit

---
+++ lib/MainHome.dart
@@ -0,0 +1,134 @@
+import 'package:agile02/page/aboutme.dart';
+import 'package:agile02/page/aboutus.dart';
+import 'package:agile02/page/homepage.dart';
+import 'package:agile02/page/listcreator.dart';
+import 'package:agile02/providers/pageProv.dart';
+import 'package:agile02/providers/provUtama.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class UtamaHome extends StatefulWidget {
+  const UtamaHome({super.key});
+
+  @override
+  State<UtamaHome> createState() => _UtamaHomeState();
+}
+
+class _UtamaHomeState extends State<UtamaHome> {
+  @override
+  Widget build(BuildContext context) {
+    final pageprov = Provider.of<PageProv>(context);
+    final user = Provider.of<ProvUtama>(context);
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: Image.asset('assets/title.png'),
+        actions: [
+          if (user.islogin != "")
+            PopupMenuButton<String>(
+              onSelected: (String value) {
+                if (user.islogin == "") {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text("Informasi"),
+                        content: const Text("Maaf, Anda harus masuk ke Akun"),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text("OK"),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                } else {
+                  // Aksi yang diambil saat menu dipilih
+                  if (value == 'home') {
+                    setState(() {
+                      pageprov.setselectedPage = 0;
+                    });
+                    Navigator.of(context).pop();
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => UtamaHome()));
+                  } else if (value == "search") {
+                    setState(() {
+                      pageprov.setselectedPage = 1;
+                    });
+                    Navigator.of(context).pop();
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => UtamaHome()));
+                  } else if (value == "logout") {
+                    logout() async {
+                      Navigator.pop(context);
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => MainHome()));
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text("Berhasil LogOut"),
+                        duration: Duration(milliseconds: 900),
+                      ));
+                      setState(() {
+                        user.setislogin = "";
+                        pageprov.setselectedPage = 0;
+                      });
+                    }
+
+                    logout();
+                  }
+                }
+              },
+              itemBuilder: (BuildContext context) => [
+                const PopupMenuItem<String>(
+                  value: 'home',
+                  child: Text('Informasi Akun'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'search',
+                  child: Text('Cari Creator'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'logout',
+                  child: Text('Logout'),
+                ),
+              ],
+            ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 10,
+        backgroundColor: Color(0xff9ED447),
+        currentIndex: pageprov.selectedPage,
+        onTap: (value) {
+          setState(() {
+            pageprov.setselectedPage = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: "Search", icon: Icon(Icons.search)),
+          BottomNavigationBarItem(
+              label: "About Us", icon: Icon(Icons.question_mark)),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Image.asset('assets/footer.png'),
+          ),
+          pageprov.selectedPage == 0
+              ? AboutMe()
+              : pageprov.selectedPage == 1
+                  ? Listacc()
+                  : pageprov.selectedPage == 2
+                      ? AboutUs()
+                      : Container()
+        ],
+      ),
+    );
+    ;
+  }
+}
---
+++ lib/page/homepage.dart
@@ -0,0 +1,191 @@
+import 'package:agile02/page/listcreator.dart';
+import 'package:agile02/page/login.dart';
+import 'package:agile02/page/register.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/provUtama.dart';
+import '../temp.dart';
+
+class MainHome extends StatefulWidget {
+  const MainHome({super.key});
+
+  @override
+  State<MainHome> createState() => _MainHomeState();
+}
+
+class _MainHomeState extends State<MainHome> {
+  TextEditingController usernameInputRegister = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<ProvUtama>(context, listen: false).updateTotalPendapatan();
+    Provider.of<ProvUtama>(context, listen: false).earlyAll();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mainprov = Provider.of<ProvUtama>(context);
+    var berbagi;
+    return Template(
+      child: SingleChildScrollView(
+        child: Stack(children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: (){
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => Template(child: Listacc())));
+                      // showDialog(
+                      //   context: context,
+                      //   builder: (BuildContext context) {
+                      //     return AlertDialog(
+                      //       title: Text("Informasi"),
+                      //       content: Text(
+                      //           "Anda harus Login untuk melihat atau mencari Creator"),
+                      //       actions: [
+                      //         TextButton(
+                      //           onPressed: () {
+                      //             Navigator.of(context).pop();
+                      //           },
+                      //           child: Text("OK"),
+                      //         ),
+                      //       ],
+                      //     );
+                      //   },
+                      // );
+                    },
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.zoom_in,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "Cari",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Color(0xff22A62F)),
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18)))),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (_) => Login()));
+                      },
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.login,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ))
+                ]),
+          ),
+          Container(
+            margin: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/logo.png"),
+                const Text("Berbagi kebaikan dengan setiap sumbangan.",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontFamily: "Quicksand")),
+                const Text.rich(TextSpan(
+                    style:
+                        TextStyle(color: Colors.white, fontFamily: "Quicksand"),
+                    children: [
+                      TextSpan(text: "dengan "),
+                      TextSpan(
+                          text: "BagiBagi,",
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(
+                          text:
+                              " kini anda dapat memberikan support ke kreator favorit anda dengan berbagai macam jenis pembayaran! mari berbagi kebaikan untuk semua di "),
+                      TextSpan(
+                          text: "BagiBagi.id",
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ])),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.white),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: TextField(
+                          controller: usernameInputRegister,
+                          decoration: InputDecoration(
+                              border: null, hintText: "bagibagi.id/"),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) => Register(
+                                          usernameInputRegister:
+                                              usernameInputRegister)));
+
+                              usernameInputRegister.clear();
+                            },
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(
+                                    Color(0xff22A62F)),
+                                shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(18)))),
+                            child: Text("Buat Akun!")),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
